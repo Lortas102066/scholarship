ed2b63488cbe7d81160f40540285764c0eb22acc
Lortas102066 <mitsukimorinaga@gmail.com>
2024-11-21 21:58:37 +1100

    Google analytics configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
+import Script from 'next/script';
 import type { Metadata } from "next";
 import { Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { NextAuthProviders } from "@/lib/next-auth/provider";
-import { Analytics } from "@vercel/analytics/react"
-
+import { Analytics } from "@vercel/analytics/react";
 
 const notoSansJP = Noto_Sans_JP({
   subsets: ["latin"],
@@ -25,14 +25,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body
-        className={`${notoSansJP.variable} font-sans antialiased`}
-      >
+      <body className={`${notoSansJP.variable} font-sans antialiased`}>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-X1PM5BBT08"
+          strategy="afterInteractive"
+        />
+        <Script id="ga-script" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-X1PM5BBT08');
+          `}
+        </Script>
         <NextAuthProviders>
           <Header />
-          <div className="pt-16">
-            {children}
-          </div>
+          <div className="pt-16">{children}</div>
           <Footer />
         </NextAuthProviders>
         <Analytics />

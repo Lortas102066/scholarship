202a594bfd82e5eb4ddd15d4e9369ab39339e618
Lortas102066 <mitsukimorinaga@gmail.com>
2024-11-21 21:36:32 +1100

    fixed analytics to before

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,3 @@
-"use client";
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import Script from 'next/script';
 import type { Metadata } from "next";
 import { Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
@@ -9,11 +5,6 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { NextAuthProviders } from "@/lib/next-auth/provider";
 
-declare global {
-  interface Window {
-    gtag: (...args: any[]) => void;
-  }
-}
 
 const notoSansJP = Noto_Sans_JP({
   subsets: ["latin"],
@@ -31,41 +22,8 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      window.gtag('config', process.env.NEXT_PUBLIC_GA_ID, {
-        page_path: url,
-      });
-    };
-
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
-
   return (
     <html lang="ja">
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
       <body
         className={`${notoSansJP.variable} font-sans antialiased`}
       >
